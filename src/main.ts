1c990ffe93745338afaf4026dbf8cd77c5c8df20
saitooooooo <kitsit.39@gmail.com>
2022-01-26 09:31:46 +0900

    add firebase-admin, nest config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { firebaseConfig } from 'src/config/firebaseConfig';
+import * as admin from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  admin.initializeApp(firebaseConfig);
   await app.listen(8000);
 }
 bootstrap();
