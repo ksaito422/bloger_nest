1f6491ee96d822305d46b94aa5794c9420bb8d35
saitooooooo <kitsit.39@gmail.com>
2022-02-13 14:45:45 +0900

    add レート制限

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ArticleModule } from './domains/article/article.module';
 import { AuthModule } from './domains/auth/auth.module';
 import { UserModule } from './domains/user/user.module';
 import { MyLoggerModule } from './loggers/myLogger.module';
 import { TypeormLoggerModule } from 'src/loggers/typeormLogger.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     ArticleModule,
     AuthModule,
@@ -16,6 +22,11 @@ import { TypeormLoggerModule } from 'src/loggers/typeormLogger.module';
     TypeormLoggerModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
